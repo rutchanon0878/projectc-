ba81d7ee77d1f368d65f7716e1ba057c0a172014
660610873-Pollapat <152590968+660610873-Pollapat@users.noreply.github.com>
2024-02-26 04:49:52 +0700

    Intro start game
    ขาด
    1.เอารวมเข้าไปในฟั่งชั่นเริ่มเกม
    2.intro , how to play ยังอาจไม่เสร็จสมบูรณ์หรือยังมีข้อผิดพลาดเช่นวิธีเล่นอาจผิดหรือ
    3.ตกแต่ง , สี , ใส้กรอบตกแต่งรอบนอก

---
+++ Game/Project intro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+#include<cstdlib>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+    cout << "Hell world ";
+
+}
