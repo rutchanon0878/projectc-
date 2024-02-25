0d88e9017c7013302ab69cdf5fd8f84d18156b50
vorapat0881 <vorapat_g@cmu.ac.th>
2024-02-25 16:50:40 +0700

    Not finished work

--- Game/INTRO.cpp
+++ Game/INTRO.cpp
@@ -8,18 +8,58 @@
 
 using namespace std; 
 
-int const scale = 5; // ขนาดของกระดาน(ลองเปลี่ยนได้)
+int const scale = 4; // ขนาดของกระดาน(ลองเปลี่ยนได้)
 
 class Unit{
     public:
         int hp = 0;
         int atk = 0;
-        string txt = " (x) ";    // ค่าตัวอักษรที่แสดงใน terminal
+        string txt = " x ";    // ค่าตัวอักษรที่แสดงใน terminal
         void txtUpdate();       // Update ตัวแปร txt อันด้านบน ↑
         void create(int, int); // Update ค่า stat ของ unit
         void damaged(int);    // ลดค่า hp ของ unit
 };
 
+class Random {
+    string file;
+    vector<string> names; 
+    vector<int> atks;      
+    vector<int> hps;
+    string sum_name[5];
+    int sum_atk[5];      
+    int sum_hp[5];       
+    
+public:
+    void importcard(const string filename);
+};
+
+void Random::importcard(const string filename) {
+    ifstream text(filename);
+    srand(time(NULL));
+    
+    while(getline(text, file)){
+        char name[10]; 
+        int attack, health;
+        sscanf(file.c_str(), "%[^:]: %d %d", name, &attack, &health);
+        names.push_back(name);
+        atks.push_back(attack);
+        hps.push_back(health);
+    }
+    int N = 3;
+    for(int i = 0;i<N ;i++){
+    int number1 = rand()% names.size();
+    int *num = new int(number1);
+    sum_name[i] = names[number1] ; 
+    sum_atk[i] = atks[number1] ; 
+    sum_hp[i] = hps[number1] ;
+
+    delete num;
+  }
+  //ทดสอบระบบ/////////
+    cout << sum_name[0] << "\n";
+    // cout << sum_atk[0] << "\n";
+    // cout << sum_hp[0] << "\n";
+}
 ////////////New create//////////////////////////////ขอสไปรท์
 void importDataFromFile(const string filename,vector<string> &names,vector<int> &atk,vector<int> &hp){
     ifstream text(filename);
@@ -70,16 +110,71 @@ void Player::attack(Player &target, int num){
         else target.slots[num].damaged(slots[num].atk);
     }
 }
+/////////////////////////////ของปอนด์เริ่มตรงนี้!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+void many(const wchar_t specialChar) {
+    for (int N=1; N<9; N++)
+    wcout << specialChar;
+}
 
+void one(const wchar_t specialChar) {
+    wcout << specialChar;
+}
+//  ____________
+// ┇✖  Gula   ✖┇
+// ┇ ╔╧╧╧╧╧╧╧╧╗ ┇
+// ┇⚔️ -<3>- ⚔️┇
+// ┇❤️️ -<4>- ❤️️┇
+// ┇ ╚╤╤╤╤╤╤╤╤╝ ┇
+// ┇____________┇
 // แสดงกระดานใน terminal
 void display(Player left, Player right){ 
-    cout << "=================================\n";
+
+    wchar_t F = L'\u00CF'; // ใช้รหัส Unicode
+    wchar_t G = L'\u00D1';
+    wchar_t A = L'\u00C9';
+    wchar_t B = L'\u00BB';
+    wchar_t C = L'\u00C8';
+    wchar_t D = L'\u00BC';
+    
+    // one(A);
+    // many(F);
+    // one(B);
+    // cout << "\n\n\n\n";
+    // one(C);
+    // many(G);
+    // one(D);
+    cout << endl;
+    cout << "====================================================================================================================================\n";
     for(int i=0; i<scale; i++){
-        cout << "|    " << left.slots[i].txt << "     <->     " << right.slots[i].txt << "    |\n"; // วนแสดง unit ที่อยู่ในแต่ละช่อง
+        // cout << "|    " << left.slots[i].txt << "     <->     " << right.slots[i].txt << "    |\n"; // วนแสดง unit ที่อยู่ในแต่ละช่อง
+        cout << "          ";
+        cout <<             " ____________" << "                                                                                        " << "____________";
+        cout << "\n          |     ZX     |" << "                                                                                      " << "|            |";
+        cout << "\n          | ";
+        one(A);
+        many(F);
+        one(B);
+        cout << " |" << "                                                                                      " << "| ";
+        one(A);
+        many(F);
+        one(B);
+        cout << " |";
+        cout << "\n          |            |" << "                                                                                      " << "|            |";
+        cout << "\n          |            |" << "                                                                                      " << "|            |";
+        cout << "\n          | ";
+        one(C);
+        many(G);
+        one(D);
+        cout << " |" << "                                                                                      " << "| ";
+        one(C);
+        many(G);
+        one(D);
+        cout << " |";
+        cout << "\n          |____________|" << "                                                                                      " << "|____________|\n";
     }
     cout << "_________________________________\n";
     cout << "|    " << left.name << "<" << left.hp << ">            " << right.name << "<" << right.hp << ">   |\n"; // บรรทัดสำหรับแสดงค่า hp
-    cout << "=================================\n";
+    cout << "====================================================================================================================================\n";
 }
 
 string toUpperStr(string x){
@@ -90,11 +185,11 @@ string toUpperStr(string x){
 
 // การรับคำสั่งของผู้เล่น
 
-string toUpperStr(string x){
-    string y = x;
-    for(unsigned i = 0; i < x.size();i++) y[i] = toupper(x[i]);
-    return y;
-}
+// string toUpperStr(string x){
+//     string y = x;
+//     for(unsigned i = 0; i < x.size();i++) y[i] = toupper(x[i]);
+//     return y;
+// }
 ///////////////////////////////////////////ของฟิว
 void action(Player &currP){
     string input;
@@ -135,7 +230,14 @@ void combat(Player &first, Player &second){
 
 int main()
 {
+
     string filename = "Namecard";
+    vector<string> nameVector;
+    vector<int> atkVector,hpVector;
+    Random random;
+    random.importcard(filename);
+    
+    
     int turn = 1;
     Player p1, p2;
     p1.name = "P1"; p2.name = "P2"; // กำหนดค่า name สำหรับเอาไว้แสดง
